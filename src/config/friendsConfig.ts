691dd55580d7fca6a0cb0074d45d772a17d7d9bb
ice-creamy405 <3910427010@qq.com>
2026-08-08 17:54:19 +0800

    友链

--- src/config/friendsConfig.ts
+++ src/config/friendsConfig.ts
@@ -23,11 +23,11 @@ export const friendsPageConfig: FriendsPageConfig = {
 // 友链配置
 export const friendsConfig: FriendLink[] = [
 	{
-		title: "夏夜流萤",
+		title: "红笺小字",
 		imgurl:
-			"https://weavatar.com/avatar/d252655d40d6874417a720bad0a6c5f77f8f6a1fd2f882f8f338402dc37e4190?s=640",
+			"https://avatars.githubusercontent.com/u/262918219?v=4&size=64",
 		desc: "飞萤之火自无梦的长夜亮起，绽放在终竟的明天。",
-		siteurl: "https://blog.cuteleaf.cn",
+		siteurl: "https://hongjianxiaozi-blog-6g8h3knwh-ice-creamy405s-projects.vercel.app/",
 		tags: ["Blog"],
 		weight: 10, // 权重，数字越大排序越靠前
 		enabled: true, // 是否启用
